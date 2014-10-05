6a884c52e6316cf5cae90c13f8e9cd12b5a5de21
ZhouQi <bitwolaiye@gmail.com>
2014-10-05 20:17:31 +0800

    fix bitheri test compile error

--- BitheriTests/Core/BTAddressTest.m
+++ BitheriTests/Core/BTAddressTest.m
@@ -17,6 +17,7 @@
 //  limitations under the License.
 
 #import <XCTest/XCTest.h>
+#import "BTAddress.h"
 #import "BTAddressManager.h"
 #import "BTTestHelper.h"
 
@@ -51,7 +52,7 @@
     //  [addressManager setWatchOnlyDir:watchOnly];
     //5KYZdUEo39z3FPrtuX2QbbwGnNP5zTd7yyr2SC1j299sBCnWjss private key
     BTKey *key = [BTKey keyWithPublicKey:[@"04a34b99f22c790c4e36b2b3c2c35a36db06226e41c692fc82b8b56ac1c540c5bd5b8dec5235a0fa8722476c7709c02559e3aa73aa03918ba2d492eea75abea235" hexToData]];
-    BTAddress *btAddress = [[BTAddress alloc] initWithKey:key encryptPrivKey:nil];
+    BTAddress *btAddress = [[BTAddress alloc] initWithKey:key encryptPrivKey:nil isXRandom:NO];
 
 
     DDLogDebug(@"address ,%@", btAddress.address);
--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
+source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '7.0'
 pod 'OpenSSL', '1.0.1'
 pod 'FMDB', '~> 2.3'

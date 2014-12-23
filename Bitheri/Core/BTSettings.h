408eab8f9f4fd00247cc27552989ac35ed7d5ac1
ZhouQi <bitwolaiye@gmail.com>
2014-12-23 10:32:20 +0800

    version 1.2.2->1.2.3

--- Bitheri/Core/BTSettings.h
+++ Bitheri/Core/BTSettings.h
@@ -98,7 +98,7 @@
 #define MSG_ALERT       @"alert"
 #define MSG_REJECT      @"reject" // described in BIP61: https://gist.github.com/gavinandresen/7079034
 
-#define BITHERI_VERSION @"1.2.2"
+#define BITHERI_VERSION @"1.2.3"
 #define BITHERI_NAME @"Bitheri"
 #define USERAGENT [NSString stringWithFormat:@"/Bither:%@/", BITHERI_VERSION]
 

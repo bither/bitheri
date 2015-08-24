57cea1a9fa29bfb2d0a666e85492f3c3775bf23e
ZhouQi <bitwolaiye@gmail.com>
2015-08-24 16:04:21 +0800

    fix bug

--- Bitheri/DatabaseProviders/BTHDAccountAddressProvider.m
+++ Bitheri/DatabaseProviders/BTHDAccountAddressProvider.m
@@ -713,8 +713,8 @@
         }
         if (syncedIndex >= addressCount) {
             sql = @"select count(0) from hd_account_addresses a,outs b "
-                    " where a.address=b.out_address and a.hd_account_id=? and a.address_index>? and a.is_synced=? and a.path_type=?";
-            rs = [db executeQuery:sql, @(hdAccountId), @(syncedIndex - addressCount + 1), @(YES), @(EXTERNAL_ROOT_PATH)];
+                    " where a.address=b.out_address and a.hd_account_id=? and a.address_index>=? and a.is_synced=? and a.path_type=?";
+            rs = [db executeQuery:sql, @(hdAccountId), @(syncedIndex - addressCount), @(YES), @(EXTERNAL_ROOT_PATH)];
             if ([rs next]) {
                 result = [rs intForColumnIndex:0] > 0;
             }
